6362f00fdf11e728ef4a5741b52ab13eb3b50717
Konga Raju <kongaraju@gmail.com>
2016-01-08 00:03:40 +0530

    initial checkin with mozilla35 useragent and platform
    1. useragent 
    2. platform
    3. commandline url
    4. load time detection

--- rawbot.js
+++ rawbot.js
@@ -1,1 +1,42 @@
 //Headless automation with PhantomJs
+"use strict";
+var page = require('webpage').create(),
+ system = require('system'),
+ address,t;
+var Mozilla35 = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:35.0) Gecko/20100101 Firefox/35.0';
+console.log('The default user agent is ' + page.settings.userAgent);
+
+if (system.args.length === 1) {
+    console.log('Usage: rawbot.js <some URL>');
+    phantom.exit(1);
+}
+
+page.onInitialized = function () {
+    page.evaluate(function () {
+
+        (function () {
+            var platform = "Linux";
+
+            window.navigator.__defineGetter__('platform', function () {
+                window.navigator.sniffed = true;
+                return platform;
+            });
+        })();
+    });
+};
+
+address = system.args[1];
+ t = Date.now();
+page.settings.userAgent = 'Mozilla35';
+page.open(, function (status) {
+    if (status !== 'success') {
+        console.log('Unable to access network');
+    } else {
+        t = Date.now() - t;
+        console.log('Page title is ' + page.evaluate(function () {
+            return document.title;
+        }));
+        console.log('Loading time ' + t + ' msec');
+    }
+    phantom.exit();
+});
