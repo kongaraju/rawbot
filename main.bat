346633e77faf1ca9e56d6c08e460ccfbea28ab74
Konga Raju <kongaraju@gmail.com>
2016-09-26 10:18:22 +0530

    Update main.bat

--- main.bat
+++ main.bat
@@ -2,5 +2,5 @@ echo OFF
 set dt=%date:-=_%_%time:~0,2%_%time:~3,2%_%time:~6,2%
 type NUL >> cookie_store/cookies_%dt%.txt
 cd ../bin
-phantomjs --proxy=127.0.0.1:9150 --proxy-type=socks5 --cookies-file=cookies_%dt%.txt ../scripts/source/ref2.js http://rk-awesome.blogspot.in/ http://stackoverflow.com/questions/11010834/how-to-run-multiple-dos-commands-in-parallel
-cd ../scripts
+phantomjs --proxy=127.0.0.1:9150 --proxy-type=socks5 --cookies-file=cookies_%dt%.txt ./source/ref2.js DESTINATION_URL REFERRER_URL
+cd ./
